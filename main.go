8f790373170d6e956bdc027745d4ea477297df43
Denis <maksimovis74@gmail.com>
2024-01-24 15:12:59 +0500

    longest Common Prefix task has been added

--- main.go
+++ main.go
@@ -99,3 +99,22 @@ func romanToInt(s string) int {
 	}
 	return sum
 }
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 1 { // handle only 1 element
+		return strs[0]
+	}
+
+	// sort them first, the most different one will be in first and last
+	sort.Strings(strs)
+
+	// compare first and last
+	l := len(strs)
+	for i := range strs[0] {
+		fmt.Println(i)
+		if strs[0][i] != strs[l-1][i] {
+			return strs[0][:i]
+		}
+	}
+	return strs[0]
+}
