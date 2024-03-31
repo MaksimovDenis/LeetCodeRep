56e27417c420ce06d0ba3f5b019f8ad0b7476c6c
Denis <maksimovis74@gmail.com>
2024-03-31 22:20:49 +0500

    new task has been added

--- main.go
+++ main.go
@@ -948,3 +948,41 @@ func joinChannels(chs ...<-chan int) <-chan int {
 	return multiplexChannel
 
 }
+
+type MyStack struct {
+	queue []int
+}
+
+func Constructor() MyStack {
+	return MyStack{}
+}
+
+func (this *MyStack) Push(x int) {
+	this.queue = append(this.queue, x)
+}
+
+func (this *MyStack) Pop() int {
+	tmp := this.queue[len(this.queue)-1]
+	this.queue = this.queue[:len(this.queue)-1]
+	return tmp
+}
+
+func (this *MyStack) Top() int {
+	return this.queue[len(this.queue)-1]
+}
+
+func (this *MyStack) Empty() bool {
+	if len(this.queue) == 0 {
+		return true
+	}
+	return false
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
